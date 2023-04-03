9031fc2168b3e1bf40053b95dacc237dd1a6ad3e
johns <johns@Johnsy>
2023-04-03 12:47:46 +0530

    1st commit

---
+++ src/test/java/CommendLine/ParameterTest.java
@@ -0,0 +1,14 @@
+package CommendLine;
+
+import org.testng.annotations.Test;
+
+public class ParameterTest {
+	@Test
+	public void login()
+	{
+	 String b=System.getProperty("browser");
+	 System.getProperty("username");
+	 System.out.println(b);
+	}
+
+}
