4d88540d6322577e46b918412364ab804fddb332
johns <johns@Johnsy>
2023-04-04 11:04:15 +0530

    Merge branch 'master' of https://github.com/JohnsyKula/Life-Insurance-Management.git

---
+++ src/test/java/Sample/Example2.java
@@ -0,0 +1,17 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Example2 {
+
+	@Test
+	public void sample3()
+	{
+		System.out.println("Sample script 3");
+	}
+	@Test
+	public void sample4()
+	{
+		System.out.println("Sample script 4");
+	}
+}
