6555d6c0e92a6dc62f4ce4a1dcc1983a0aa2c67a
johns <johns@Johnsy>
2023-04-04 10:10:10 +0530

    script1

---
+++ src/test/java/Sample/Example1.java
@@ -0,0 +1,19 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Example1 {
+
+	@Test
+	public void sample1()
+	{
+		System.out.println("Sample script 1");
+
+	}
+
+	public void sample2()
+	{
+		System.out.println("Sample script 2");
+
+	}
+}
